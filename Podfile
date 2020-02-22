8921ec7adfcad39a78fb5c57c54f0c98f5af1333
takuma.beppu <takuma.beppu@gmail.com>
2020-02-22 15:22:04 +0900

    pod installと.gitignoreの作成まで実施

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.1'
+
+target 'taskapp2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for taskapp2
+  pod 'RealmSwift', '3.20.0'
+
+end
---
+++ taskapp2/InputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InputViewController.swift
+//  taskapp2
+//
+//  Created by t on 2020/02/22.
+//  Copyright © 2020 takuma.beppu. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
